006fb2caed55ff6aef2abf81f7eb3af8488ea812
Levi Porter <levi@levibrooke.com>
2019-07-26 16:50:33 -0700

    Fix jsx markup in home, register

--- src/home.js
+++ src/home.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 const home = () => (
   <div id="landing-page">
-    <div class="container" id="landing-content">
+    <div className="container" id="landing-content">
       <h2>Welcome to YourPackageIsHere</h2>
-      <p>Making deliveries that much <span class="special">easier</span>.</p>
+      <p>Making deliveries that much <span className="special">easier</span>.</p>
 
       {/* If not logged in: */}
       <span>
-        <p>Join <span class="special">now</span> to start using!</p>
-        <a class="btn btn-dark btn-xl" href="/register">Sign up</a>
-        <a class="btn btn-dark btn-xl" href="/login">Login</a>
+        <p>Join <span className="special">now</span> to start using!</p>
+        <a className="btn btn-dark btn-xl" href="/register">Sign up</a>
+        <a className="btn btn-dark btn-xl" href="/login">Login</a>
       </span>
 
   </div>
--- src/register.js
+++ src/register.js
@@ -1,52 +1,56 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
-export default (props) => {
+class Register extends Component {
 
-  return (
-    <Fragment>
-      <h3 class="text-center">Register Account</h3>
+  render() {
+    return (
+      <Fragment>
+        <h3 className="text-center">Register Account</h3>
 
-      <form method="POST" action="/buildingcreate">
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="username">Username</label>
-            <input name="username" id="username" class="form-control" />
+        <form method="POST" action="/buildingcreate">
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="username">Username</label>
+              <input name="username" id="username" className="form-control" />
+            </div>
+            <div className="form-group col-md-6">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" className="form-control" />
+            </div>
           </div>
-          <div class="form-group col-md-6">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" class="form-control" />
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" className="form-control" />
+            </div>
+            <div className="form-group col-md-6">
+              <label htmlFor="name">Building Name</label>
+              <input name="name" id="name" className="form-control" />
+            </div>
           </div>
-        </div>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" class="form-control" />
+          <div className="form-group">
+            <label htmlFor="streetaddress">Street Address</label>
+            <input name="streetaddress" id="streetaddress" className="form-control" />
           </div>
-          <div class="form-group col-md-6">
-            <label for="name">Building Name</label>
-            <input name="name" id="name" class="form-control" />
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="city">City</label>
+              <input name="city" id="city" className="form-control" />
+            </div>
+            <div className="form-group col-md-4">
+              <label htmlFor="state">State</label>
+              <input name="state" id="state" className="form-control" />
+            </div>
+            <div className="form-group col-md-2">
+              <label htmlFor="zip">Zip Code</label>
+              <input name="zip" id="zip" className="form-control" />
+            </div>
           </div>
-        </div>
-        <div class="form-group">
-          <label for="streetaddress">Street Address</label>
-          <input name="streetaddress" id="streetaddress" class="form-control" />
-        </div>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="city">City</label>
-            <input name="city" id="city" class="form-control" />
-          </div>
-          <div class="form-group col-md-4">
-            <label for="state">State</label>
-            <input name="state" id="state" class="form-control" />
-          </div>
-          <div class="form-group col-md-2">
-            <label for="zip">Zip Code</label>
-            <input name="zip" id="zip" class="form-control" />
-          </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Register </button>
-      </form>
-    </Fragment>
-  )
+          <button type="submit" className="btn btn-primary">Register </button>
+        </form>
+      </Fragment>
+    )
+  }
 }
+
+export default Register;
